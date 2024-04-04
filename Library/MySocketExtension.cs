fe0da771761925a454cf09c795f20b612ee85618
Kostiantyn Polishko <kostantinpolishko@gmail.com>
2024-04-04 23:18:47 +0300

    2. In class MySocketExtension realized method to estimate size of msg in bytes in sentMsg() and inform it to getMsg().

--- Library/MySocketExtension.cs
+++ Library/MySocketExtension.cs
@@ -9,18 +9,32 @@ namespace LibSC
 {
     public static class MySocketExtension
     {
-        public static void getMsg(in Socket socket, out string msg)
-        {
-            byte[] bytes = new byte[1024];
-            int bytesRead = socket.Receive(bytes);
-            msg = Encoding.Unicode.GetString(bytes, 0, bytesRead);
-        }
-
         public static void sentMsg(in Socket socket, out string? msg, string? txt = default)
         {
             Console.Write($"Enter msg {(txt != null ? "(| exit | - " + txt + ")" : string.Empty)}: ");
             msg = Console.ReadLine();
+
+            socket.Send(IntToBytes(msg));
             socket.Send(Encoding.Unicode.GetBytes(msg ??= "no msg"));
+        }
+
+        public static void getMsg(in Socket socket, out string msg)
+        {
+            byte[] bytes = new byte[32];
+            socket.Receive(bytes);
+
+            byte[] data = new byte[BitConverter.ToInt32(bytes, 0)];
+            int bytesRead = socket.Receive(data);
+            msg = Encoding.Unicode.GetString(data, 0, bytesRead);
+        }
+
+        private static byte[] IntToBytes(string? msg)
+        {
+            byte[] bytes = BitConverter.GetBytes(Encoding.Unicode.GetByteCount(msg ??= "no msg"));
+
+            if (BitConverter.IsLittleEndian) { Array.Reverse(bytes); }
+
+            return bytes;
         }
     }
 }
